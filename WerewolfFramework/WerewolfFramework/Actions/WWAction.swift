d6e01c3d1dc942fef0a6bec52d4976361e5a787b
Adam Gastineau <adam@appcannonsoftware.com>
2017-01-07 18:56:53 -0500

    Fixed Git error, adding WWCharacter and WWAction

---
+++ WerewolfFramework/WerewolfFramework/Actions/WWAction.swift
@@ -0,0 +1,13 @@
+//
+//  WWAction.swift
+//  WerewolfFramework
+//
+//  Created by Adam Gastineau on 1/7/17.
+//  Copyright © 2017 Adam Gastineau. All rights reserved.
+//
+
+import Foundation
+
+class WWAction {
+	
+}
---
+++ WerewolfFramework/WerewolfFramework/Characters/WWCharacter.swift
@@ -0,0 +1,37 @@
+//
+//  WWCharacter.swift
+//  WerewolfFramework
+//
+//  Created by Adam Gastineau on 1/7/17.
+//  Copyright © 2017 Adam Gastineau. All rights reserved.
+//
+
+import Foundation
+
+class WWCharacter {
+	
+	enum TurnOrder {
+		case concurrent
+		case last
+		case inactive
+	}
+	
+	var name: String
+	var image: UIImage?
+	var turnOrder: TurnOrder
+	
+	var instructions: String
+	
+	init() {
+		self.name = "Default Character"
+		self.turnOrder = .inactive
+		self.instructions = "Default character instructions (PLEASE REPLACE)"
+	}
+	
+	/**
+		Interprets the provided WWAction (typically created from the GUI) and mutates the WWState
+	*/
+	func perform(action: WWAction, with state: WWState) {
+		print("[WARNING] Default action performed. Nothing was changed")
+	}
+}
