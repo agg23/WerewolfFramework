144a2bf53f4c36c719c076e7363f82596d0a476d
Adam Gastineau <adam@appcannonsoftware.com>
2017-01-11 11:21:25 -0500

    Added WWRobber

---
+++ WerewolfFramework/WerewolfFramework/Characters/WWRobber.swift
@@ -0,0 +1,62 @@
+//
+//  WWRobber.swift
+//  WerewolfFramework
+//
+//  Created by Adam Gastineau on 1/11/17.
+//  Copyright © 2017 Adam Gastineau. All rights reserved.
+//
+
+import Foundation
+
+public class WWRobber: WWCharacter {
+	public init() {
+		super.init(name: "Robber", instructions: "I am a Robber", turnOrder: .concurrent, orderNumber: 19, selectable: .humanOnly, interactionCount: 1, canSelectSelf: false, defaultVisible: [], defaultViewable: .none)
+		self.selectionComplete = true
+	}
+	
+	public required init?(coder decoder: NSCoder) {
+		super.init(coder: decoder)
+	}
+	
+	override public func perform(action: WWAction, with state: WWState) {
+		let firstActionData = action.actions[0]
+		
+		guard let first = firstActionData.firstSelection else {
+			print("[WARNING] Invalid WWActionData for Robber")
+			return
+		}
+
+		guard let newCharacter = state.assignments[first] else {
+			print("[WARNING] Invalid player for Robber")
+			return
+		}
+		
+		for (playerIndex, character) in state.assignments {
+			if character == self {
+				state.assignments[playerIndex] = newCharacter
+				break
+			}
+		}
+		
+		state.assignments[first] = self
+	}
+	
+	public override func received(action: WWAction, state: WWState) -> Bool {
+		let firstActionData = action.actions[0]
+		
+		guard let first = firstActionData.firstSelection else {
+			print("[WARNING] Invalid WWActionData for Robber")
+			return false
+		}
+		
+		if let character = state.assignments[first] {
+			self.seenAssignments[first] = type(of: character)
+		} else {
+			print("[WARNING] Invalid selected character for Robber")
+		}
+		
+		self.selectionComplete = true
+		
+		return true
+	}
+}
